686016b1220b83820b156eccae0f4ed394fbec46
GURU SABARISH <gurusabarisha@gmail.com>
2020-07-28 01:50:26 -0400

    Delete listexist.py

--- listexist.py
+++
@@ -1,8 +0,0 @@
-lst=[1,2,3,4,5,6,7,8,9,0]
-exist=int(input("Enter your element :"))
-for i in lst:
-    if exist==i:
-        print("Your input element is exist in the list")
-        break
-else:
-    print("Your input element is not exist in the list")
