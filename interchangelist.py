193c14c55df76e75e98199c3bbb6076e521df6b7
GURU SABARISH <gurusabarisha@gmail.com>
2020-07-28 01:42:37 -0400

    Delete interchangelist.py

--- interchangelist.py
+++
@@ -1,8 +0,0 @@
-lis = []
-n=int(input("How many nnumbers want to insert in a list? "))
-        
-for i in range(n):
-    b=input("Enter the element :")
-    lis.append(b)
-lis[0],lis[(len(lis)-1)]=lis[(len(lis)-1)],lis[0]
-print(lis)
