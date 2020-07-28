296ccb272938a2b3f4a7f9d1697f40714d934da3
GURU SABARISH <gurusabarisha@gmail.com>
2020-07-28 01:51:30 -0400

    Delete listmulti.py

--- listmulti.py
+++
@@ -1,9 +0,0 @@
-lst=[]
-n=int(input("How many numbers want to insert in the list :"))
-for i in range(n):
-    a=int(input())
-    lst.append(a)
-multi=1
-for i in lst:
-    multi=multi*i
-print("Multiplication of the elements in list =",multi)
