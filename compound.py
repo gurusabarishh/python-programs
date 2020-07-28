c7b272a9c8dd51c96c5b467f83c193df2e9f7420
GURU SABARISH <gurusabarisha@gmail.com>
2020-07-28 01:44:56 -0400

    Delete compound.py

--- compound.py
+++
@@ -1,7 +0,0 @@
-principle = float(input("principle :"))
-time = float(input("time : "))
-rate = float(input("rate : "))
-a= (1+(rate/100))**time
-a= pow(1+(rate/100),time)
-compound = principle*a
-print("compound intrest = ",compound)
