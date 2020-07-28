89e37899cf042e3b317a71226974ba86a6ce0155
GURU SABARISH <gurusabarisha@gmail.com>
2020-07-28 01:54:13 -0400

    Delete oddeven.py

--- oddeven.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter the starting number :"))
-num2 = int(input("Enter the end number :"))
-for i in range(num1,num2+1):
-    if i%2 == 0:
-        print (i,"is even number")
-    else:
-        print (i,"is odd number")
